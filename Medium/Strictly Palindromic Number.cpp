d3a6e95cceb8245fa4c63a8fea59faa56fbd9d2f
chibi-hariesh <164385243+chibi-hariesh@users.noreply.github.com>
2024-07-30 00:32:14 +0530

    30-7-24

---
+++ Medium/Strictly Palindromic Number.cpp
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/strictly-palindromic-number/description/
+
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        if(n==0) return true;
+        return false;
+    }
+};
