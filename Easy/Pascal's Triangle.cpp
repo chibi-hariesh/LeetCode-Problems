3855b5c7335e5d90d80d14eb57d7b2c39d6579e0
chibi-hariesh <164385243+chibi-hariesh@users.noreply.github.com>
2024-07-30 23:55:22 +0530

    30-7-24

---
+++ Easy/Pascal's Triangle.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/pascals-triangle/description/
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+
+        vector <vector<int>> v1(numRows);
+        for (int i=0;i<numRows;i++){
+            v1[i].resize(i+1);
+            v1[i][i] = v1[i][0] = 1;
+            for(int j=1;j<i;j++){
+                v1[i][j] = v1[i-1][j-1] + v1[i-1][j];
+            }
+        }
+        return v1;
+    }
+};
