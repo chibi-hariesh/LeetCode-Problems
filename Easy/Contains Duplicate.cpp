d58014c4e5fe983cb9ba033e1b44cee7877856d9
chibi-hariesh <164385243+chibi-hariesh@users.noreply.github.com>
2024-08-12 15:51:09 +0530

    12-8-24

--- Easy/Contains Duplicate.cpp
+++ Easy/Contains Duplicate.cpp
@@ -1,1 +1,32 @@
 //https://leetcode.com/problems/contains-duplicate/description/
+//solution 1:
+class Solution {
+public:
+    bool containsDuplicate(std::vector<int>& nums) {
+        std::unordered_set<int> seen;
+        for (int x : nums) {
+            if (seen.find(x) != seen.end()) {
+                return true;
+            }
+            seen.insert(x);
+        }
+        return false;
+    }
+};
+
+//solution 2:
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> map;
+        for(int x : nums){
+            if(map.find(x) != map.end()){
+                return true;
+            }
+            else{
+                map[x]++;
+            }
+        }
+        return false;
+    }
+};
